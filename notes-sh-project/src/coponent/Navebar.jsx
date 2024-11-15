6750d2e210f61c5c7d5bebeb5bc004322db70a0d
kumarmanojgit <krmanoj72099@gmail.com>
2024-11-15 18:00:22 +0530

    add navbar

--- notes-sh-project/src/coponent/Navebar.jsx
+++ notes-sh-project/src/coponent/Navebar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navebar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="flex flex-row gap-4">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/Pastes">Pastes</NavLink>
+    </div>
+  );
 };
 
 export default Navebar;
