f26c68b481e72909d83e4e369f9336aa5407d30b
kumarmanojgit <krmanoj72099@gmail.com>
2024-11-15 19:55:33 +0530

    add button

--- notes-sh-project/src/coponent/Home.jsx
+++ notes-sh-project/src/coponent/Home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom"; // Ensure you have this import if using react-router-dom
 
 const Home = () => {
-  return <div>home</div>;
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
+  //const [searchParams, setSearchParams] = useState("");
+  const [searchParams] = useSearchParams(); //useSearchParams hook
+  const pastId = searchParams.get("pasteId");
+  return (
+    <div className="flex flex-row gap-7">
+      <input
+        className="p-2 rounded-2xl mt-2"
+        type="text"
+        placeholder="Enter your paste Name!"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button className="p-2 rounded-2xl mt-2 text-white">
+        {pastId ? "Update" : "Create"}
+      </button>
+    </div>
+  );
 };
 
 export default Home;
--- notes-sh-project/src/coponent/Navebar.jsx
+++ notes-sh-project/src/coponent/Navebar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Navebar = () => {
   return (
-    <div className="flex flex-row gap-4">
+    <div className="flex flex-row gap-4 place-content-evenly">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/Pastes">Pastes</NavLink>
     </div>
